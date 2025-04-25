c68565ca24e70e7413c9fcb706350ba3e9f2c098
Annie Barkan <anniebarkan121@gmail.com>
2025-04-25 16:43:47 -0400

    Update payloadUpload.ino

--- Air-Quality-Sensor-master 2/CSL-AQS-V4/payloadUpload.ino
+++ Air-Quality-Sensor-master 2/CSL-AQS-V4/payloadUpload.ino
@@ -1,8 +1,8 @@
 /*
-  Write to Google Sheets through a Wifi  HTTP1.1 request to a Google script.
+  Write to Google Sheets through a Wifi HTTP request to GitHub Pages proxy.
 */
 void payloadUpload(String payload) {
-  for (int i = 1; i < 4; i++) { // allways try to connect to wifi
+  for (int i = 1; i < 4; i++) { // always try to connect to wifi
     if (passcodeg != "") // if password is not empty
       status = WiFi.begin(ssidg, passcodeg);
     else
@@ -16,16 +16,17 @@ void payloadUpload(String payload) {
       Serial.print("payload: ");
       payload = payload_base + String("\"") + payload + String("\"}");
       Serial.println(payload);
+      
+      // URL encode the payload
+      String encodedPayload = urlEncode(payload);
+      
       // Make a HTTP request:
-      client.println(String("POST /airquality/?gsid=") + String(gsidg) + String(" HTTP/1.1"));
-      client.println("Host: https://annie1212011.github.io");
-      client.println("Content-Type: application/x-www-form-urlencoded");
-      //client.println("Connection: close");
-      client.print("Content-Length: ");
-      client.println(payload.length());
+      client.println("GET /airquality?gsid=" + String(gsidg) + "&payload=" + encodedPayload + " HTTP/1.1");
+      client.println("Host: annie1212011.github.io");
+      client.println("User-Agent: Arduino/1.0");
+      client.println("Connection: close");
       client.println();
-      client.print(payload);
-      client.println();
+      
       delay(200);
 
       if (client.available())
@@ -52,25 +53,43 @@ void payloadUpload(String payload) {
 
 void initializeClient() {
   Serial.print("\nStarting connection to server... ");
-  if (client.connectSSL(server, 443)) {      // if you get a connection, report back via serial:
+  if (client.connectSSL(server, 443)) {
     Serial.print("Connected to "); Serial.println(server);
-    while (client.available()) {
-      char c = client.read();
-      Serial.write(c);
-    }
-    client.print("GET /your-repo-name/?gsid=");
-    client.print(gsidg);
-    client.println(" HTTP/1.1");
-    
-    client.print("Host: ");
-    client.println(server);
-    client.println("User-Agent: Arduino/1.0");
-    client.println("Connection: close");
-    client.println(); // Empty line to end headers
+    // Initial connection is successful, don't need to do anything else here
   }
   else {
     Serial.print("Not connected to ");
     Serial.println(server);
   }
   Serial.println("end intializeClient");
+}
+
+// URL encoding function
+String urlEncode(String str) {
+  String encodedString = "";
+  char c;
+  char code0;
+  char code1;
+  for (int i = 0; i < str.length(); i++) {
+    c = str.charAt(i);
+    if (c == ' ') {
+      encodedString += '+';
+    } else if (isAlphaNumeric(c)) {
+      encodedString += c;
+    } else {
+      code1 = (c & 0xf) + '0';
+      if ((c & 0xf) > 9) {
+        code1 = (c & 0xf) - 10 + 'A';
+      }
+      c = (c >> 4) & 0xf;
+      code0 = c + '0';
+      if (c > 9) {
+        code0 = c - 10 + 'A';
+      }
+      encodedString += '%';
+      encodedString += code0;
+      encodedString += code1;
+    }
+  }
+  return encodedString;
 }
